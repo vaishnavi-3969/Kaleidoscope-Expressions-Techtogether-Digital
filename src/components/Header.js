da38ad2c72621e1aa213f188178815f6f27dc085
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-24 21:02:48 +0530

    homepage and sidebar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { FiMenu } from 'react-icons/fi';
-import { FaPalette } from 'react-icons/fa';
+import { FiMenu, FiUser, FiLogOut, FiX } from 'react-icons/fi';
+import { FaPalette, FaHome, FaRegNewspaper, FaUserAlt, FaShoppingBag, FaRegComment, FaRegHeart, FaFire } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const sidebarVariants = {
+    open: {
+      x: 0,
+      transition: {
+        duration: 0.5,
+        type: "spring",
+        stiffness: 100,
+        damping: 15
+      }
+    },
+    closed: {
+      x: -300,
+      transition: {
+        duration: 0.5,
+        type: "spring",
+        stiffness: 100,
+        damping: 15
+      }
+    }
+  };
+
   return (
     <header className="bg-gradient-to-r from-purple-900 to-indigo-800 py-4 px-6">
       <nav className="flex items-center justify-between">
@@ -13,7 +45,10 @@ const Header = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <FiMenu className="text-white text-2xl mr-4 cursor-pointer" />
+            <FiMenu
+              className="text-white text-2xl mr-4 cursor-pointer"
+              onClick={toggleSidebar}
+            />
           </motion.div>
           <motion.div
             initial={{ opacity: 0, x: -10 }}
@@ -30,6 +65,93 @@ const Header = () => {
         >
           <FaPalette className="text-white text-2xl cursor-pointer" />
         </motion.div>
+
+        {isSidebarOpen && (
+          <motion.div
+            className="fixed top-0 left-0 h-screen w-64 bg-purple-900 text-white flex flex-col p-4"
+            variants={sidebarVariants}
+            initial="closed"
+            animate="open"
+          >
+            <div className="flex items-center mb-8">
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center mr-4">
+                <FiUser className="text-purple-800 text-xl" />
+              </div>
+              <h2 className="text-lg font-semibold">Hey, Vaishnavi</h2>
+            </div>
+            <ul className="space-y-2">
+              <li>
+                <Link to="/" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FaHome className="w-5 h-5 mr-2" />
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/exhibitions" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FaRegNewspaper className="w-5 h-5 mr-2" />
+                  Exhibitions
+                </Link>
+              </li>
+              <li>
+                <Link to="/notifications" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FiLogOut className="w-5 h-5 mr-2" />
+                  Notifications
+                </Link>
+              </li>
+              <li>
+                <Link to="/profile" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FaUserAlt className="w-5 h-5 mr-2" />
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link to="/marketplace" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FaShoppingBag className="w-5 h-5 mr-2" />
+                  Marketplace
+                </Link>
+              </li>
+              <li>
+                <Link to="/artists" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FaUserAlt className="w-5 h-5 mr-2" />
+                  Artists
+                </Link>
+              </li>
+              <li>
+                <Link to="/collections" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FaRegNewspaper className="w-5 h-5 mr-2" />
+                  Collections
+                </Link>
+              </li>
+              <li>
+                <Link to="/chat" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FaRegComment className="w-5 h-5 mr-2" />
+                  Chat
+                </Link>
+              </li>
+              <li>
+                <Link to="/favorites" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FaRegHeart className="w-5 h-5 mr-2" />
+                  Favorites
+                </Link>
+              </li>
+              <li>
+                <Link to="/trending" className="flex items-center px-6 py-2 text-gray-300 hover:text-white">
+                  <FaFire className="w-5 h-5 mr-2" />
+                  Trending
+                </Link>
+              </li>
+            </ul>
+            <motion.button
+              className="text-white mt-auto flex items-center cursor-pointer"
+              onClick={closeSidebar}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              <FiX className="mr-2" />
+              Close
+            </motion.button>
+          </motion.div>
+        )}
       </nav>
     </header>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,124 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
+import Logo from '../assets/logo.png';
 
-export default function Home() {
+const Home = () => {
+  const features = [
+    {
+      title: 'Exhibitions',
+      description: 'Explore the latest art exhibitions',
+      icon: '🖼️',
+      path: '/exhibitions',
+    },
+    {
+      title: 'Notifications',
+      description: 'Stay updated with notifications',
+      icon: '🔔',
+      path: '/notifications',
+    },
+    {
+      title: 'Profile',
+      description: 'Manage your profile and preferences',
+      icon: '👤',
+      path: '/profile',
+    },
+    {
+      title: 'Marketplace',
+      description: 'Buy and sell artworks',
+      icon: '💲',
+      path: '/marketplace',
+    },
+    {
+      title: 'Artists',
+      description: 'Discover talented artists',
+      icon: '🎨',
+      path: '/artists',
+    },
+    {
+      title: 'Events',
+      description: 'Attend art-related events',
+      icon: '🎉',
+      path: '/events',
+    },
+    {
+      title: 'Collections',
+      description: 'Create and explore art collections',
+      icon: '🗃️',
+      path: '/collections',
+    },
+    {
+      title: 'Chat',
+      description: 'Connect with other art enthusiasts',
+      icon: '💬',
+      path: '/chat',
+    },
+    {
+      title: 'Favorites',
+      description: 'Save your favorite artworks',
+      icon: '⭐',
+      path: '/favorites',
+    },
+    {
+      title: 'Trending',
+      description: 'Discover trending artworks',
+      icon: '📈',
+      path: '/trending',
+    },
+  ];
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className="min-h-screen bg-gray-900 py-8 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-4xl font-bold text-white mb-8">Welcome to </h1>
+        <div className="container mx-auto px-4 py-8">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="flex items-center justify-center mb-8"
+      >
+         <div className="w-35 h-35 flex items-center justify-center rounded-full bg-white">
+            <img src={Logo} alt="Logo" className="h-24 w-24" />
+          </div>
+        <h1 className="text-4xl md:text-6xl font-bold text-yellow-300">
+          <Typewriter
+            options={{
+              strings: ['KaleidoscopExpressions'],
+              autoStart: true,
+              loop: true,
+              delay: 80,
+              deleteSpeed: 50,
+            }}
+          />
+        </h1>
+      </motion.div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          {features.map((feature, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.2 }}
+              className="bg-gray-800 rounded-lg p-6 shadow-lg cursor-pointer"
+            >
+              <div className="text-5xl mb-4">{feature.icon}</div>
+              <h2 className="text-xl font-semibold mb-2 text-white">{feature.title}</h2>
+              <p className="text-gray-400">{feature.description}</p>
+              <Link
+                to={feature.path}
+                className="mt-4 inline-block bg-purple-900 text-white px-4 py-2 rounded-lg"
+              >
+                View
+              </Link>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Home;
