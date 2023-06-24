b45b267b8e83d05f33bf0cdfde00fa0ece08121e
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-24 12:19:41 +0530

    intialized

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './components/Home';
+import Exhibition from "./components/Exhibition";
+import Notifications from "./components/Notifications";
+import ProfileCard from "./components/ProfileCard";
+
 
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home/>} exact/>
+        <Route path="/exhibitions" element={<Exhibition/>} exact/>
+        <Route path="/notifications" element={<Notifications/>} exact/>
+        <Route path="/profile" element={<ProfileCard/>} exact/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Exhibition.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Exhibition() {
+  return (
+    <div>Exhibition</div>
+  )
+}
