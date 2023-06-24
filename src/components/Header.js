20a8fb930ab1cd9ff655dacaa1c1bb4a5f71f841
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-24 12:32:34 +0530

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { FiMenu } from 'react-icons/fi';
+import { FaPalette } from 'react-icons/fa';
 
-export default function Header() {
+const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="bg-gradient-to-r from-purple-900 to-indigo-800 py-4 px-6">
+      <nav className="flex items-center justify-between">
+        <div className="flex items-center">
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <FiMenu className="text-white text-2xl mr-4 cursor-pointer" />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -10 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <h1 className="text-white text-2xl font-semibold">KaleidoscopExpressions</h1>
+          </motion.div>
+        </div>
+        <motion.div
+          initial={{ opacity: 0, x: 10 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <FaPalette className="text-white text-2xl cursor-pointer" />
+        </motion.div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
