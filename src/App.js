463e46a41c96285c71b72cff22392a8f12c6cba4
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-24 12:25:20 +0530

    basic features added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import Home from './components/Home';
 import Exhibition from "./components/Exhibition";
 import Notifications from "./components/Notifications";
 import ProfileCard from "./components/ProfileCard";
+import Marketplace from "./components/Marketplace";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Artists from "./components/Artists";
 
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header/>
         <Routes>
         <Route path="/" element={<Home/>} exact/>
         <Route path="/exhibitions" element={<Exhibition/>} exact/>
         <Route path="/notifications" element={<Notifications/>} exact/>
         <Route path="/profile" element={<ProfileCard/>} exact/>
+        <Route path="/marketplace" element={<Marketplace/>} exact/>
+        <Route path="/artists" element={<Artists/>} exact/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
